70410b3be74834091bc2bddf1e9d58602b6dfc08
stormlin <lin-jinting@outlook.com>
2019-12-31 17:29:59 +0800

    可以生成节点入度列表和节点加载顺序列表

--- har-files/priority.py
+++ har-files/priority.py
@@ -35,41 +35,54 @@ def command_remove(argv):
     save_har_file(content_field_removed, output_file_name)
 
 
-def get_deps(item):
+def get_call_stack(item):
     """
     深度优先搜索，搜索到的所有依赖项都会被添加到 set 类型实例 deps 中。每一个 item
     对象均包含 callFrames 数组和 parent 对象。如果 parent 对象非空，则继续在
     parent 对象中递归寻找依赖项。如果不包含 parent 对象，则只需查找 callFrames
     对象中的内容。返回值是依赖项列表
     """
-    result = set()
+    result = []
     # 查找 callFrames 中包含的依赖项
     call_frames = item['callFrames']
-    deps_from_call_frames = set()
+    deps_from_call_frames = []
     for frame in call_frames:
-        deps_from_call_frames.add(frame['url'])
-    for dep in deps_from_call_frames:
-        result.add(dep)
-    # result.extend(list(deps_from_call_frames))
+        deps_from_call_frames.append(frame['url'])
+    result.extend(deps_from_call_frames)
 
     if 'parent' in item:
         # 含有 parent 对象，需要在此对象中进行递归查找
-        deps_from_parent = get_deps(item['parent'])
-        for dep in deps_from_parent:
-            result.add(dep)
-        # result.extend(list(deps_from_parent))
-    return list(result)
+        deps_from_parent = get_call_stack(item['parent'])
+        result.extend(deps_from_parent)
+    return result
+
+
+def get_file_call_sequence(call_stack_list):
+    """
+    从调用栈列表中提取文件调用顺序
+
+    Args:
+      call_stack_list: 调用栈列表
+
+    Returns:
+      从调用栈最深到最浅的文件调用顺序列表
+    """
+    call_stack_list.reverse()
+    file_call_sequence = []
+    added_files = set()
+    for file_url in call_stack_list:
+        if file_url not in added_files:
+            file_call_sequence.append(file_url)
+            added_files.add(file_url)
+    return file_call_sequence
 
 
 def generate_priority_tree(har_file_json):
     entries = har_file_json['log']['entries']
-    # num_entries = len(entries)
 
     result = []
-
     root_request_url = ''
 
-    # print('num_entries: %d' % num_entries)
     for entry in entries:
         extracted_entry_info = {}
 
@@ -79,59 +92,178 @@ def generate_priority_tree(har_file_json):
 
         response = entry['response']
         status = response['status']
-        # size = response['content']['size']
+        size = response['content']['size']
         # mime_type = response['content']['mimeType']
-        # priority = entry['_priority']
+        priority = entry['_priority']
         resource_type = entry['_resourceType']
 
         if root_request_url == '':
             root_request_url = url
-            # print('root request found, url = <%s>' % url)
         initiator = entry['_initiator']
 
         extracted_entry_info['url'] = url
+        extracted_entry_info['size'] = size
+        extracted_entry_info['priority'] = priority
         extracted_entry_info['resource_type'] = resource_type
 
         if status != 404 and url.find('https://www.stormlin.com') >= 0:
-            # print('url = <%s>, priority = <%s>, resource_type = <%s>'
-            #       % (url, priority, resource_type))
             has_stack = 'stack' in initiator
             initiator_type = initiator['type']
-            # print('url = <%s>, type = <%s>, stack = <%s>' % (url, intiator['type'], has_stack))
-            # print('extracting deps for url = <%s>, type = <%s>, has_stack = <%s>' % (url, initiator_type, has_stack))
             extracted_entry_info['initiator_type'] = initiator_type
-            # print('generate_priority_tree: %s, initiator_type = <%s>' % (url, initiator_type))
             if initiator_type == 'parser' or initiator_type == 'other':
-                # print('  0. %s' % root_request_url)
                 extracted_entry_info['deps'] = [root_request_url]
                 result.append(extracted_entry_info)
                 continue
             if has_stack:
                 # find all deps by deep first search
-                deps_list = get_deps(initiator['stack'])
+                deps_list = get_call_stack(initiator['stack'])
+                # 从函数调用栈中提取文件调用顺序
                 extracted_entry_info['deps'] = deps_list
                 result.append(extracted_entry_info)
 
     return result
 
 
+def sort_leaf_nodes_by_type_and_size(leaf_nodes):
+    """
+    对关键路径中的叶子节点按照资源类型和大小进行排序
+
+    Args:
+      leaf_nodes: 叶子节点列表
+
+    Returns:
+      排好序的叶子节点列表
+    """
+    document_list = []
+    stylesheet_list = []
+    script_list = []
+
+    for resource in leaf_nodes:
+        resource_type = resource['resource_type']
+        if resource_type == 'document':
+            document_list.append(resource)
+            continue
+        if resource_type == 'stylesheet':
+            stylesheet_list.append(resource)
+            continue
+        if resource_type == 'script':
+            script_list.append(resource)
+            continue
+
+    document_list.sort(key=lambda element: element['size'])
+    stylesheet_list.sort(key=lambda element: element['size'])
+    script_list.sort(key=lambda element: element['size'])
+
+    result = []
+    result.extend(document_list)
+    result.extend(stylesheet_list)
+    result.extend(script_list)
+
+    return result
+
+
 def command_priority(argv):
     har_file_path = argv[1]
-    output_file_name = argv[3]
     har_file_json = parse_har_file(har_file_path)
     priority_info_list = generate_priority_tree(har_file_json)
-    entry_count = 1
-    for info in priority_info_list:
-        resource_type = info['resource_type']
-        if resource_type not in ['document', 'script', 'stylesheet']:
+
+    # 筛选出关键路径中的资源
+    critical_path = []
+    for resource in priority_info_list:
+        resource_type = resource['resource_type']
+        if resource_type in ['document', 'script', 'stylesheet']:
+            critical_path.append(resource)
             continue
-        print('%s. url = <%s>, resource_type = <%s>, initiator_type = <%s>' %
-              (entry_count, info['url'], info['resource_type'], info['initiator_type']))
-        dep_count = 1
-        for dep in info['deps']:
-            print('  %s. url = <%s>' % (dep_count, dep))
-            dep_count += 1
-        entry_count += 1
+
+    # 计算每一个文件的网络调用顺序
+    for resource in critical_path:
+        file_call_sequence = get_file_call_sequence(resource['deps'])
+        resource['file_call_sequence'] = file_call_sequence
+
+    # 计算文件在依赖图中的入度，入度越大越有可能影响后续渲染过程
+    indegree_dict = {}
+    for resource in critical_path:
+        # 初始化
+        indegree_dict[resource['url']] = 0
+
+    for resource in critical_path:
+        file_call_sequence = resource['file_call_sequence']
+        for call in file_call_sequence:
+            if call not in indegree_dict:
+                indegree_dict[call] = 0
+            else:
+                indegree_dict[call] = indegree_dict[call] + 1
+
+    # 添加到数据源中
+    for resource in critical_path:
+        url = resource['url']
+        resource['indegree'] = indegree_dict[url]
+
+    print('indegree list')
+    # 区分叶子节点和非叶子节点
+    indegree_intermediate_nodes = []
+    indegree_leaf_nodes = []
+    for resource in critical_path:
+        if resource['indegree'] ==0:
+            # 叶子节点的入度为 0
+            indegree_leaf_nodes.append(resource)
+            continue
+        # 中间节点的入度不为 0
+        indegree_intermediate_nodes.append(resource)
+
+    # todo: 找不到 order
+    indegree_intermediate_nodes.sort(key=lambda element: element['indegree'], reverse=True)
+    for resource in indegree_intermediate_nodes:
+        print('  indegree = <%.1f>, url = <%s>' % (resource['indegree'], resource['url']))
+    indegree_leaf_nodes = sort_leaf_nodes_by_type_and_size(indegree_leaf_nodes)
+    for resource in indegree_leaf_nodes:
+        print('  leaf node, size = <%.1f>KB, url = <%s>' % (resource['size'] / 1000.0, resource['url']))
+
+    # 计算文件在加载过程中的平均位置，越小越靠前，应当优先传输
+    print('load order list')
+    load_order = {}
+    for resource in critical_path:
+        # 初始化相关结构体
+        url = resource['url']
+        load_order[url] = {
+            'order': 0,
+            'count': 0
+        }
+
+    for resource in critical_path:
+        url = resource['url']
+        index = 1
+        for call in resource['file_call_sequence']:
+            # 计算该文件的相对加载顺序
+            load_order[call]['order'] += index
+            load_order[call]['count'] += 1
+            index += 1
+
+    for resource in critical_path:
+        url = resource['url']
+        resource['order'] = load_order[url]['order']
+        resource['count'] = load_order[url]['count']
+
+    # 区分叶子节点和非叶子节点
+    load_order_intermediate_nodes = []
+    load_order_leaf_nodes = []
+    for resource in critical_path:
+        url = resource['url']
+        count = load_order[url]['count']
+        if count == 0:
+            # 叶子节点不会出现在其他资源的调用栈中
+            load_order_leaf_nodes.append(resource)
+            continue
+        load_order_intermediate_nodes.append(resource)
+
+    load_order_intermediate_nodes.sort(key=lambda element: element['order'] / element['count'])
+    for resource in load_order_intermediate_nodes:
+        url = resource['url']
+        order = load_order[url]['order']
+        count = load_order[url]['count']
+        print('  average_order = <%.1f>, url = <%s>' % (order / count, url))
+    for resource in load_order_leaf_nodes:
+        print('  leaf node, size = <%.1f>KB, url = <%s>' % (resource['size'] / 1000.0, resource['url']))
 
 
 def main(argv):
